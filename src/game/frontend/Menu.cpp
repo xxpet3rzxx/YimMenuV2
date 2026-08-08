3baf0912b553c2172a8d160e4fb74ff8698055a3
PETROZ <peterpankung@hotmail.com>
2026-08-08 13:44:38 +0700

    Add Thai font support
    Add Thai glyph support to the ImGui font atlas using Tahoma and GetGlyphRangesThai().

--- src/game/frontend/Menu.cpp
+++ src/game/frontend/Menu.cpp
@@ -82,6 +82,7 @@ namespace YimMenu
 		FontCfg.MergeMode = true;
 		io.Fonts->AddFontFromFileTTF((std::filesystem::path(std::getenv("SYSTEMROOT")) / "Fonts" / "arial.ttf").string().c_str(), size, &FontCfg, GetGlyphRangesCyrillicOnly());
 		io.Fonts->AddFontFromFileTTF((std::filesystem::path(std::getenv("SYSTEMROOT")) / "Fonts" / "meiryo.ttc").string().c_str(), size, &FontCfg, io.Fonts->GetGlyphRangesJapanese());
+		io.Fonts->AddFontFromFileTTF((std::filesystem::path(std::getenv("SYSTEMROOT")) / "Fonts" / "tahoma.ttf").string().c_str(), size, &FontCfg, io.Fonts->GetGlyphRangesThai());
 
 		io.Fonts->Build();
 

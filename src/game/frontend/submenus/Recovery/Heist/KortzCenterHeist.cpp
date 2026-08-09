c67a8dff642e44fe892cde64d35ea2ace86c1100
PETROZ <peterpankung@hotmail.com>
2026-08-09 10:32:17 +0700

    Add finish primary

--- src/game/frontend/submenus/Recovery/Heist/KortzCenterHeist.cpp
+++ src/game/frontend/submenus/Recovery/Heist/KortzCenterHeist.cpp
@@ -59,10 +59,9 @@ namespace YimMenu::Submenus
 		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheistcutglass"_J));
 		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheistdisablelaser"_J));
 		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheisttakeprimary"_J));
+		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheistfinishprimary"_J));
 		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheistenablesolosecondary"_J));
 		inHeist->AddItem(std::make_shared<CommandItem>("kortzcenterheisttakesecondary"_J));
-
-		action->AddItem(std::make_shared<CommandItem>("kortzcenterheistsetup"_J));
 
 		tab->AddItem(target);
 		tab->AddItem(general);
